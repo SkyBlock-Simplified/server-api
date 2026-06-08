0cf1f9c53bb7f9aecaddc8e4526439c59551a7c5
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-06-08 09:54:04 -0400

    Pin gson-extras to b68510e and client to 64ae978

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ dependencies {
     testRuntimeOnly(libs.spring.boot.properties.migrator)
 
     // Exported dependencies (available to consumers)
-    api("com.github.simplified-dev:gson-extras") { version { strictly("26278a3") } }
-    api("com.github.simplified-dev:client") { version { strictly("c368e5f") } }
+    api("com.github.simplified-dev:gson-extras") { version { strictly("b68510e") } }
+    api("com.github.simplified-dev:client") { version { strictly("64ae978") } }
     api(libs.gson)
     api(libs.spring.boot.actuator)
     api(libs.spring.boot.web)
