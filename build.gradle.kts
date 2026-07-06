566c20ac4dcbf542705479f1020164e05bb73bd9
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-07-06 04:52:28 -0400

    Re-pin client to 5a5d32e (jitpack.yml rebuild)

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
 
     // Exported dependencies (available to consumers)
     api("com.github.simplified-dev:gson-extras") { version { strictly("f42ee07") } }
-    api("com.github.simplified-dev:client") { version { strictly("92c40b0") } }
+    api("com.github.simplified-dev:client") { version { strictly("5a5d32e") } }
     api(libs.gson)
     api(libs.spring.boot.actuator)
     api(libs.spring.boot.web)
